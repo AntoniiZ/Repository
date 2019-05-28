0701eb70cff02c3413197794077d36ac228826ae
Martin Stamenkov <bul2013@abv.bg>
2019-05-28 18:06:37 +0300

    script for the database tables

---
+++ final.sql
@@ -0,0 +1,28 @@
+Drop database if exists Vignettes;
+Create database Vignettes CHARSET 'utf8';
+use Vignettes;
+
+Create table Vignettes_data(
+	Id INT primary key auto_increment,
+    Regnum VARCHAR(8) not null,
+    CategoryId INT not null,
+    CountryId INT not null,
+    Valid_from datetime not null,
+    Valid_to datetime not null,
+    
+    FOREIGN key (CountryId) references Countries(CountryId) on update cascade,
+    foreign key (CategoryId) references Categories(CategoryID) on update cascade
+);
+
+create table Countries(
+	CountryId INT primary key auto_increment,
+    Name VARCHAR(50) not null
+);
+
+create table Categories(
+	CategoryId INT primary key auto_increment,
+    Name varchar(10) not null
+);
+
+
+
