5e2046c140f63cb4e609346be7d3c59206a0d907
Martin Stamenkov <bul2013@abv.bg>
2019-05-29 22:55:16 +0300

    added CRUD operations for Categories table

---
+++ create_category.php
@@ -0,0 +1,79 @@
+<?php
+require_once "config.php";
+ 
+$name = "";
+$name_err = $address_err = $salary_err = "";
+ 
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    // Validate name
+    $input_name = trim($_POST["name"]);
+    if(empty($input_name)){
+        $name_err = "Please enter a name.";
+    } elseif(!filter_var($input_name, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z\s]+$/")))){
+        $name_err = "Please enter a valid name.";
+    } else{
+        $name = $input_name;
+    }
+    
+    if(empty($name_err)){
+        $sql = "INSERT INTO Categories (Name) VALUES (?)";
+         
+        if($stmt = mysqli_prepare($link, $sql)){
+            
+            mysqli_stmt_bind_param($stmt, "s", $param_name);
+            
+            $param_name = $name;
+            
+            if(mysqli_stmt_execute($stmt)){
+                
+                header("location: index.php");
+                exit();
+            } else{
+                echo "Something went wrong. Please try again later.";
+            }
+        }
+         
+        mysqli_stmt_close($stmt);
+    }
+    
+    mysqli_close($link);
+}
+?>
+ 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Create Record</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        .wrapper{
+            width: 500px;
+            margin: 0 auto;
+        }
+    </style>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="page-header">
+                        <h2>Create Record</h2>
+                    </div>
+                    <p>Enter country name</p>
+                    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                        <div class="form-group <?php echo (!empty($name_err)) ? 'has-error' : ''; ?>">
+                            <label>Name</label>
+                            <input type="text" name="name" class="form-control" value="<?php echo $name; ?>">
+                            <span class="help-block"><?php echo $name_err;?></span>
+                        </div>
+                        <input type="submit" class="btn btn-primary" value="Submit">
+                        <a href="index.php" class="btn btn-default">Cancel</a>
+                    </form>
+                </div>
+            </div>        
+        </div>
+    </div>
+</body>
+</html>
---
+++ read_category.php
@@ -0,0 +1,71 @@
+<?php
+if(isset($_GET["id"]) && !empty(trim($_GET["id"]))){
+
+    require_once "config.php";
+    
+    $sql = "SELECT * FROM Categories WHERE CategoryId = ?";
+    
+    if($stmt = mysqli_prepare($link, $sql)){
+        mysqli_stmt_bind_param($stmt, "i", $param_id);
+        
+        $param_id = trim($_GET["id"]);
+        
+        if(mysqli_stmt_execute($stmt)){
+            $result = mysqli_stmt_get_result($stmt);
+    
+            if(mysqli_num_rows($result) == 1){
+                $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+                
+                // Retrieve individual field value
+                $name = $row["Name"];
+            } else{
+                header("location: error.php");
+                exit();
+            }
+            
+        } else{
+            echo "Oops! Something went wrong. Please try again later.";
+        }
+        mysqli_stmt_close($stmt);
+    }
+    else echo "peda;";
+
+    mysqli_close($link);
+} else{
+    header("location: error.php");
+    exit();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>View Record</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        .wrapper{
+            width: 500px;
+            margin: 0 auto;
+        }
+    </style>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="page-header">
+                        <h1>View recorded categories</h1>
+                    </div>
+                    <div class="form-group">
+                        <label>Name</label>
+                        <p class="form-control-static"><?php echo $row["Name"]; ?></p>
+                    </div>
+                    <p><a href="index.php" class="btn btn-primary">Back</a></p>
+                </div>
+            </div>        
+        </div>
+    </div>
+</body>
+</html>
